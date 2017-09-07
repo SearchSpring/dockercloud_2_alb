8ad14f826bb69e12f066db4b9923ddba70a3eb1c
Greg Hellings <greg@thesub.net>
2017-09-07 15:08:37 -0700

    Fixing typo

--- script.sh
+++ script.sh
@@ -64,7 +64,7 @@ i=0
 n=0
 echo -e "Comparing IDs\n"
 for ID in ${!COMPARE[@]}; do
-  echo "$ID : ${CO  MPARE[$ID]}"
+  echo "$ID : ${COMPARE[$ID]}"
   if [ ${COMPARE[$ID]} = 1 ]; then
     REMOVEID[$i]=$ID
     i=$(($i + 1))
